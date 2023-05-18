b7b4245ed9ab16292e172960198469abea3abb14
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-18 14:39:09 -0500

    Get results from pytorch models

--- dnn_dependencies/hfExample_Pytorch.py
+++ dnn_dependencies/hfExample_Pytorch.py
@@ -1,14 +1,22 @@
+from collections import OrderedDict
 from pathlib import Path
 
 import torch
+from torch.nn import Embedding, Module, Sequential
+from torchinfo import summary
+from transformers import AutoModel, BertModel
 
 MODEL: Path = Path("../models/pytorch_model.bin")
 
 
 def main() -> None:
-    model = torch.load(f=MODEL)
+    model: Module = AutoModel.from_pretrained(
+        pretrained_model_name_or_path="../models/"
+    )
 
     print(model)
+
+    # print(type(modules))
 
 
 if __name__ == "__main__":
