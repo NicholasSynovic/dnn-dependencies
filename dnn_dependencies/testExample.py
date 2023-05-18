51b3a3e453f3561b68f658d233b66d23c6ddc274
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-18 09:37:08 -0500

    Using a custom keras model, print out JSON summary of it

--- dnn_dependencies/testExample.py
+++ dnn_dependencies/testExample.py
@@ -1,24 +1,17 @@
-# Given a protobuf Graph model, print out each layer within the model using
-
-from json import load
 from pathlib import Path
 
-from keras.models import load_model, model_from_json
+from keras.engine.sequential import Sequential
+from keras.models import load_model
 
-MODEL: Path = Path("../models/bert_base_uncased.h5")
+MODEL: Path = Path("../models/veaaCNN.h5")
 CONFIG: Path = Path("../models/config.json")
 
 
 def main() -> None:
-    with open(file=CONFIG, mode="r") as modelConfig:
-        mc: str = modelConfig.read()
-        modelConfig.close()
+    model: Sequential = load_model(filepath=MODEL)
 
-    # model = model_from_json(json_string=mc)
-    model = load_model(filepath=MODEL)
-    print(type(model))
-
-    pass
+    with open(file="test.json", mode="w") as jsonModel:
+        jsonModel.write(model.to_json(indent=4))
 
 
 if __name__ == "__main__":
