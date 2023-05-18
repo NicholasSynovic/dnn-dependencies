44e3f925d0f2c9416397a35f43ac04039071249a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-18 10:23:53 -0500

    Add dependency

---
+++ dnn_dependencies/hfExample_Pytorch.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import torch
+
+MODEL: Path = Path("../models/")
+
+
+def main() -> None:
+    model = torch.load()
+
+
+if __name__ == "__main__":
+    main()
--- dnn_dependencies/testExample.py
+++ dnn_dependencies/testExample.py
@@ -4,13 +4,12 @@ from keras.engine.sequential import Sequential
 from keras.models import load_model
 
 MODEL: Path = Path("../models/veaaCNN.h5")
-CONFIG: Path = Path("../models/config.json")
 
 
 def main() -> None:
     model: Sequential = load_model(filepath=MODEL)
 
-    with open(file="test.json", mode="w") as jsonModel:
+    with open(file="veaaCNN_Summary.json", mode="w") as jsonModel:
         jsonModel.write(model.to_json(indent=4))
 
 
