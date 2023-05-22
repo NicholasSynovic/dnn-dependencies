7f2f4b625c465d27b8b02e534e071062d70df239
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-22 09:40:29 -0500

    Change folder names

--- dnn_dependencies/hfExample_Pytorch.py
+++ dnn_dependencies/hfExample_Pytorch.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from torch.nn import Module
 from transformers import AutoModel
 
-MODEL: Path = Path("../models/")
+BERT_BASE_UNCASE: Path = Path("../models/bert_base_uncased")
+BERT_BASE_UNCASE_REUSE: Path = Path("../models/bert_base_uncased_reuse")
 
 
 def main() -> None:
-    model: Module = AutoModel.from_pretrained(pretrained_model_name_or_path=MODEL)
+    model: Module = AutoModel.from_pretrained(
+        pretrained_model_name_or_path=BERT_BASE_UNCASE
+    )
 
     print(model)
 
