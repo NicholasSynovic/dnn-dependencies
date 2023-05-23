ae0c71c612a6868056bd0161c314ce35efda07ba
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-23 15:10:30 -0500

    Use the auto preprocessor

--- dnn_dependencies/torch2onnx.bash
+++ dnn_dependencies/torch2onnx.bash
@@ -2,4 +2,4 @@
 
 # Takes in user input to either a file path or a HuggingFace repository name and an output filename
 
-python -m transformers.onnx -m $1 --framework pt --preprocessor tokenizer $2
+python -m transformers.onnx -m $1 --framework pt --preprocessor auto $2
