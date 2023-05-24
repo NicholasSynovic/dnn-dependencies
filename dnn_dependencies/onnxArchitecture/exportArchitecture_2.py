ab8a89201be710edf733fe6c2109a5d9fa43dad7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-24 15:17:26 -0500

    Refactor the original file

--- dnn_dependencies/onnxArchitecture/exportArchitecture_2.py
+++ dnn_dependencies/onnxArchitecture/exportArchitecture_2.py
@@ -1,55 +1,107 @@
 from argparse import Namespace
-from collections import namedtuple
 from itertools import count
-from json import dumps
-from pathlib import Path
 from typing import List
 
 import pandas
-from onnx import GraphProto, ModelProto, NodeProto, load
+from lxml import etree
+from onnx import load
+from onnx.onnx_pb import GraphProto, ModelProto, NodeProto
 from pandas import DataFrame
 from progress.bar import Bar
 
 from dnn_dependencies.args.generalArgs import getArgs
 
-OUTPUT_ID_COUNTER: count = count()
+NODE_ID_COUNTER: count = count()
 OUTPUT_DF_LIST: List[DataFrame] = []
 
 
-def buildOutputDF(output: str) -> DataFrame:
-    data: dict[str, List[str]] = {
-        "ID": [OUTPUT_ID_COUNTER.__next__()],
-        "Output": [output],
+def buildDF(nodeID: int, name: str, inputs: List[str], outputs: List[str]) -> DataFrame:
+    data: dict[str, List[int | str | List[str]]] = {
+        "ID": [nodeID],
+        "Name": [name],
+        "Inputs": [inputs],
+        "Outputs": [outputs],
     }
     return DataFrame(data)
+
+
+def buildXML(df: DataFrame) -> None:
+    rootNode = etree.Element("gexf")
+    rootNode.set("xmlns", "http://www.gexf.net/1.3")
+    # rootNode.set("xmlns:viz", "http://www.gexf.net/1.3/viz")
+    rootNode.set("version", "1.3")
+
+    graphNode = etree.SubElement(rootNode, "attributes")
+
+    attributesNode = etree.SubElement(graphNode, "attributes")
+    attributesNode.set("class", "node")
+
+    inputAttributeNode = etree.SubElement(attributesNode, "attribute")
+    inputAttributeNode.set("id", "input")
+    inputAttributeNode.set("name", "Input")
+    inputAttributeNode.set("type", "string")
+
+    outputAttributeNode = etree.SubElement(attributesNode, "attribute")
+    outputAttributeNode.set("id", "output")
+    outputAttributeNode.set("name", "Output")
+    outputAttributeNode.set("type", "string")
+
+    verticesNode = etree.SubElement(graphNode, "nodes")
+
+    with Bar("Creating GEXF nodes...", max=df.shape[0]) as bar:
+        for ID, NAME, INPUTS, OUTPUTS in df.itertuples(index=False):
+            vertexNode = etree.SubElement(verticesNode, "node")
+            vertexNode.set("id", str(ID))
+            vertexNode.set("title", NAME)
+
+            attvaluesNode = etree.SubElement(vertexNode, "attvalues")
+
+            i: str
+            for i in INPUTS:
+                attvalueNode = etree.SubElement(attvaluesNode, "attvalue")
+                attvalueNode.set("for", "input")
+                attvalueNode.set("value", i)
+
+            o: str
+            for o in OUTPUTS:
+                attvalueNode = etree.SubElement(attvaluesNode, "attvalue")
+                attvalueNode.set("for", "output")
+                attvalueNode.set("value", o)
+
+            bar.next()
+
+    tree = etree.ElementTree(rootNode)
+    tree.write("architecture.gexf", pretty_print=True, encoding="utf-8")
 
 
 def main() -> None:
     args: Namespace = getArgs(
         programName="ONNX Architecture Exporter",
-        description="A tool to export an ONNX model's layer architecture to a JSON file (architecture.json)",
+        description="A tool to export an ONNX model's layer architecture to a GEXF file (architecture.gexf)",
     )
 
     model: ModelProto = load(f=args.model[0])
     graph: GraphProto = model.graph
 
-    node: NodeProto
-    for node in graph.node:
-        outputs: List[str] = list(node.output)
+    with Bar(
+        "Extracting information from ONNX computational graph...", max=len(graph.node)
+    ) as bar:
+        node: NodeProto
+        for node in graph.node:
+            nodeID: int = NODE_ID_COUNTER.__next__()
+            name: str = node.name
+            outputs: List[str] = list(node.output)
+            inputs: List[str] = list(node.input)
+            df: DataFrame = buildDF(
+                nodeID=nodeID,
+                name=name,
+                inputs=inputs,
+                outputs=outputs,
+            )
+            OUTPUT_DF_LIST.append(df)
+            bar.next()
 
-        output: str
-        for output in outputs:
-            OUTPUT_DF_LIST.append(buildOutputDF(output=output))
-
-    outputDF: DataFrame = pandas.concat(objs=OUTPUT_DF_LIST, ignore_index=True)
-    outputDF.drop_duplicates(subset=["Output"], inplace=True)
-    print(outputDF)
-
-    test: str = dumps({})
-
-    with open("architecture.json", "w") as jsonFile:
-        jsonFile.writelines(test)
-        jsonFile.close()
+    buildXML(df=pandas.concat(OUTPUT_DF_LIST))
 
 
 if __name__ == "__main__":
