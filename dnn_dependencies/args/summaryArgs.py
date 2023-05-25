c3dd49d9563820e17dc19f9d6b0a44fd02902811
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-25 10:49:40 -0500

    Rename file

---
+++ dnn_dependencies/args/summaryArgs.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser, Namespace
+from importlib.metadata import version
+
+from dnn_dependencies import args as argVars
+
+
+def getArgs(programName: str, description: str = "") -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog=programName,
+        description=description,
+        epilog=f"Created by: {', '.join(argVars.authorsList)}",
+        formatter_class=argVars.AlphabeticalOrderHelpFormatter,
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{programName}: {version(distribution_name='dnn-dependencies')}",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        nargs=1,
+        default="bert-base-uncased",
+        type=str,
+        required=False,
+        help="Path to or HuggingFace repository name to utilize model from",
+    )
+    return parser.parse_args()
