2e24ddf9add1a66942f6b5b13dc5571634eacc4e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-05-25 10:55:18 -0500

    Add architecture exporter specific arguements

---
+++ dnn_dependencies/args/architectureArgs.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser, Namespace
+from importlib.metadata import version
+from pathlib import Path
+
+from dnn_dependencies import args as argVars
+
+
+def getArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog="ONNX Layer Architecture GEXF Exporter",
+        description="A program to export an ONNX model's layer architecture as a GEXF file",
+        epilog=f"Created by: {', '.join(argVars.authorsList)}",
+        formatter_class=argVars.AlphabeticalOrderHelpFormatter,
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"{programName}: {version(distribution_name='dnn-dependencies')}",
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        nargs=1,
+        type=str,
+        required=True,
+        help="Path to an ONNX model",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        nargs=1,
+        type=Path,
+        default=Path("architecture.gexf"),
+        required=False,
+        help="Filepath to store GEXF output",
+    )
+    return parser.parse_args()
