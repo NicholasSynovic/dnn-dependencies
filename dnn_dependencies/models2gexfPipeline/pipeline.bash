de48d4ade9fab5534edef42745401d6b71a8f20d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-12 16:05:20 -0500

    Update pipeline to use optimum

--- dnn_dependencies/models2gexfPipeline/pipeline.bash
+++ dnn_dependencies/models2gexfPipeline/pipeline.bash
@@ -2,6 +2,6 @@
 
 python3.10 downloadModelJSONList.py
 
-cat baseModels.txt | xargs -I % python3.10 -m transformers.onnx --model % --framework pt --preprocessor auto %.onnx
+cat baseModels.txt | xargs -I % optimum-cli export onnx --model % --framework pt %.onnx
 
 cat baseModels.txt | xargs -I % dnn-dependencies-onnx2gexf --mode production --model %.onnx/model.onnx --output %.gexf
