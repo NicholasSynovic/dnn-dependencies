036ec1eae4efe8d9fb2e677024ee3c690398b349
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-12 12:33:18 -0500

    Format modelFS.py

--- dnn_dependencies/graphAnalysis/modelFS.py
+++ dnn_dependencies/graphAnalysis/modelFS.py
@@ -6,25 +6,27 @@ from lxml import etree
 
 from dnn_dependencies.args.modelFSArgs import getArgs
 
-args = getArgs()
-# Load the GEXF file
-gexfFilePath: Path = Path(args.input[0])
 
-tree = etree.parse(gexfFilePath)
+def main() -> None:
+    args: Namespace = getArgs()
+    ns = {"gexf": "http://www.gexf.net/1.2draft"}
 
-# Define the namespace
-ns = {"gexf": "http://www.gexf.net/1.2draft"}
+    gexfFilePath: Path = Path(args.input[0])
+    tree = etree.parse(gexfFilePath)
+    nodes = tree.findall(".//gexf:node", namespaces=ns)
 
-# Select all 'node' elements
-nodes = tree.findall(".//gexf:node", namespaces=ns)
+    updatedNodes: list[Path] = [
+        Path(args.root[0], gexfFilePath.stem + node.attrib.get("label"))
+        for node in nodes
+    ]
+
+    p: Path
+
+    for p in updatedNodes:
+        directoryComponent: Path = Path(p.parent)
+        os.makedirs(directoryComponent, exist_ok=True)
+        open(file=p, mode="w")
 
 
-updatedNodes: list[str] = [
-    Path(args.root[0], gexfFilePath.stem + node.attrib.get("label")) for node in nodes
-]
-
-p: Path
-for p in updatedNodes:
-    directoryComponent: Path = Path(p.parent)
-    os.makedirs(directoryComponent, exist_ok=True)
-    open(file=p, mode="w")
+if __name__ == "__main__":
+    main()
