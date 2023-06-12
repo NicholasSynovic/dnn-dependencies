00b7a843efbb1755689d716fd3660cbe0e3218c9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-12 12:49:19 -0500

    Simplify code for node label extraction

--- dnn_dependencies/graphAnalysis/modelFS.py
+++ dnn_dependencies/graphAnalysis/modelFS.py
@@ -1,19 +1,37 @@
 import os
 from argparse import Namespace
 from pathlib import Path
+from typing import List
 
-from lxml import etree
+from bs4 import BeautifulSoup, ResultSet, Tag
+from progress.bar import Bar
 
 from dnn_dependencies.args.modelFSArgs import getArgs
 
 
+def extractNodeLabels(dom: str) -> List[Path]:
+    data: List[Path] = []
+
+    soup: BeautifulSoup = BeautifulSoup(markup=dom, features="lxml")
+    nodeTags: ResultSet = soup.findAll(name="node")
+
+    with Bar("Extacting node labels... ", max=len(nodeTags)) as bar:
+        tag: Tag
+        for tag in nodeTags:
+            data.append(Path(tag.get(key="label")))
+            bar.next()
+
+    return data
+
+
 def main() -> None:
     args: Namespace = getArgs()
-    ns = {"gexf": "http://www.gexf.net/1.2draft"}
 
-    gexfFilePath: Path = Path(args.input[0])
-    tree = etree.parse(gexfFilePath)
-    nodes = tree.findall(".//gexf:node", namespaces=ns)
+    with open(file=args.input[0], mode="r") as xmlDoc:
+        xmlDOM: str = xmlDoc.read()
+        xmlDoc.close()
+
+    nodeLabels: List[Path] = extractNodeLabels(dom=xmlDOM)
 
     updatedNodes: list[Path] = [
         Path(args.root[0], gexfFilePath.stem + node.attrib.get("label"))
