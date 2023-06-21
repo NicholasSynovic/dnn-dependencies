f278a34e89e2eb51b08a1c4cef36769f18d5afe9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-21 10:22:01 -0500

    Import args from seperate file

--- dnn_dependencies/converters/gexf2dot.py
+++ dnn_dependencies/converters/gexf2dot.py
@@ -1,27 +1,9 @@
-from argparse import ArgumentParser, Namespace
+from argparse import Namespace
 
 from networkx import Graph, read_gexf
 from networkx.drawing.nx_pydot import write_dot
 
-
-def getArgs() -> Namespace:
-    parser: ArgumentParser = ArgumentParser(
-        prog="GEXF 2 Dot file converter",
-        description="A small tool to convert a GEXF file (for Gephi) to Dot (for Graphviz)",
-    )
-    parser.add_argument(
-        "-i",
-        "--input",
-        required=True,
-        help="Input GEXF file",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        required=True,
-        help="Output Dot file location path",
-    )
-    return parser.parse_args()
+from dnn_dependencies.args.gexf2dot_args import getArgs
 
 
 def main() -> None:
