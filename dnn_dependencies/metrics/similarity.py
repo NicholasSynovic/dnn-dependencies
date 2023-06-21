ebef46ecdca45ba5da3f25eeaa1bc35e151b9e9c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-06-21 12:00:40 -0500

    Start work on general degree measurment

--- dnn_dependencies/metrics/similarity.py
+++ dnn_dependencies/metrics/similarity.py
@@ -1,8 +1,11 @@
 from argparse import Namespace
+from collections import defaultdict
 from typing import List, Set
 
 from networkx import DiGraph, read_gexf
 from networkx.algorithms.community import louvain_communities
+from networkx.classes.reportviews import NodeDataView, NodeView
+from progress.bar import Bar
 
 from dnn_dependencies.args.similarity_args import getArgs
 
@@ -20,10 +23,33 @@ def countCommunities(graph: DiGraph) -> int:
     return len(communities)
 
 
+def computeDegreeDistribution(graph: DiGraph, inDegree: bool = True) -> dict[int, int]:
+    data: defaultdict = defaultdict(int)
+    degreeType: str = "in" if inDegree else "out"
+
+    nodes: NodeView = graph.nodes()
+
+    with Bar(
+        f"Computing the distribution of {degreeType} degree nodes... ", max=len(nodes)
+    ) as bar:
+        node: str
+        for node in nodes:
+            degree: int = graph.out_degree(node)
+            data[degree] += 1
+            bar.next()
+
+    data: dict[int, int] = dict(data)
+    data = dict(sorted(data.items()))
+
+    return data
+
+
 def main() -> None:
     args: Namespace = getArgs()
 
     graph: Digraph = read_gexf(args.input[0])
+
+    computeDistribution_OutDegree(graph)
 
 
 if __name__ == "__main__":
