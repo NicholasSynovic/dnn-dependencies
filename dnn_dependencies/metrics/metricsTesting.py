1b95cc03f3fc4f639258dc2703d4bcdf07fed0e7
Karolina Ryzka <ryzkakarolina@gmail.com>
2023-07-06 12:48:36 -0500

    unit testing again

--- dnn_dependencies/metrics/metricsTesting.py
+++ dnn_dependencies/metrics/metricsTesting.py
@@ -4,13 +4,14 @@ import networkx as nx
 import similarity
 
 
-class testComputeDensity(unittest.TestCase):
+class metricsTesting(unittest.TestCase):
     def testComputeDensity(self):
         G: nx.DiGraph = nx.read_gexf(
             "/Users/karolinaryzka/Documents/dnn-dependencies/dnn_dependencies/onnxArchitecture/gpt2.gexf"
         )
         actual = similarity.computeDensity(G)
 
+        self.assertNotEqual(actual, (-1 * actual))
         self.assertNotEqual(actual, 0.5)
         self.assertEqual(actual, 0.00036878179081766563)
 
@@ -22,7 +23,7 @@ class testComputeDensity(unittest.TestCase):
 
         self.assertNotEqual(actual, 30)
         self.assertEqual(actual, 3104)
-        self.assertNotEqual(actual, -3)
+        self.assertNotEqual(actual, (-1 * actual))
 
     def testCountEdges(self):
         G: nx.DiGraph = nx.read_gexf(
@@ -31,7 +32,7 @@ class testComputeDensity(unittest.TestCase):
         actual = similarity.countEdges(G)
 
         self.assertNotEqual(actual, 3000)
-        self.assertNotEqual(actual, -3000)
+        self.assertNotEqual(actual, (-1 * actual))
         self.assertEqual(actual, 3552)
 
     def testCountCommunities(self):
@@ -41,7 +42,7 @@ class testComputeDensity(unittest.TestCase):
         actual = similarity.countCommunities(G)
 
         self.assertNotEqual(actual, 40)
-        self.assertNotEqual(actual, -40)
+        self.assertNotEqual(actual, (-1 * actual))
         self.assertEqual(actual, 1170)
 
     def testComputeDegreeDistribution(self):
@@ -51,7 +52,7 @@ class testComputeDensity(unittest.TestCase):
         actual = similarity.computeDegreeDistribution(G)
 
         self.assertNotEqual(actual, {0: 30, 1: 20, 2: 90, 3: 20, 4: 122, 5: 20})
-        self.assertNotEqual(actual, {0: -30, 1: -20, 2: -90, 3: -20, 4: -122, 5: -20})
+        self.assertNotEqual(actual, {key: -value for key, value in actual.items()})
         self.assertEqual(actual, {0: 1227, 1: 589, 2: 1069, 3: 74, 4: 122, 5: 23})
 
     def testComputeClusteringCoefficient(self):
@@ -70,16 +71,7 @@ class testComputeDensity(unittest.TestCase):
                 0.5: 20,
             },
         )
-        self.assertNotEqual(
-            actual,
-            {
-                0: -3029,
-                0.03333333333333333: -1,
-                0.05: -23,
-                0.08333333333333333: -26,
-                0.5: -25,
-            },
-        )
+        self.assertNotEqual(actual, {key: -value for key, value in actual.items()})
         self.assertEqual(
             actual,
             {
