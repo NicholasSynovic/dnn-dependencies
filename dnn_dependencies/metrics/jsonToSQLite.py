bbe3106f52e62d687b1bd9b6b96818625b9ecf88
Karolina Ryzka <ryzkakarolina@gmail.com>
2023-07-13 15:36:18 -0500

    first json to SQLite

---
+++ dnn_dependencies/metrics/jsonToSQLite.py
@@ -0,0 +1,23 @@
+import json
+import sqlite3
+
+import pandas as pd
+
+
+def convertJson(jsonFile: str) -> pd.DataFrame:
+    return pd.read_json(jsonFile)
+
+
+def convertDf(databaseFile: str, tableName: str, df: pd.DataFrame) -> None:
+    sqlite = sqlite3.connect(databaseFile)
+    return df.to_sql(tableName, sqlite, if_exists="replace")
+
+
+def main() -> None:
+    df = convertJson(
+        "/Users/karolinaryzka/Documents/dnn-dependencies/dnn_dependencies/metrics/example.json"
+    )
+    convertDf("", "", df)
+
+
+main()
