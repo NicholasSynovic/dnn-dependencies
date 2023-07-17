43770c6410dd7059ee0284044d3a0e4be6ab9b88
Karolina Ryzka <ryzkakarolina@gmail.com>
2023-07-17 10:23:43 -0500

    sql database

--- dnn_dependencies/metrics/jsonToSQLite.py
+++ dnn_dependencies/metrics/jsonToSQLite.py
@@ -10,14 +10,16 @@ def convertJson(jsonFile: str) -> pd.DataFrame:
 
 def convertDf(databaseFile: str, tableName: str, df: pd.DataFrame) -> None:
     sqlite = sqlite3.connect(databaseFile)
-    return df.to_sql(tableName, sqlite, if_exists="replace")
+
+    df.to_sql(tableName, sqlite, if_exists="replace")
+    sqlite.close()
 
 
 def main() -> None:
     df = convertJson(
         "/Users/karolinaryzka/Documents/dnn-dependencies/dnn_dependencies/metrics/example.json"
     )
-    print(convertDf("example.db", "example", df))
+    print(convertDf("example.db", "fruit", df))
 
 
 main()
