b4bea341e8139482a17b646db3b5de9185d410db
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-07-20 11:23:15 -0500

    Update script to convert onnx models to gexf files

--- dnn_dependencies/converters/automateConversion.bash
+++ dnn_dependencies/converters/automateConversion.bash
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 #Error message for no arguments
-if [[ "$#" -ne 2 ]]; then
-    echo "Usage: $0 inputPath outputPath"
-    exit 1
-fi
-
-#input output paths
-inputPath="$1"
-outputPath="$2"
-
+# if [[ "$#" -ne 2 ]]; then
+#     echo "Usage: $0 inputPath outputPath"
+#     exit 1
+# fi
 
 #Iterate through each onnx file in input folder
-for inputFile in "$inputPath"/*; do
-    if [ -f "$inputFile" ]; then
-        #get filename w/o extension and save as gexf
-        filename=$(basename "$inputFile")
-        outputFile="$outputPath/${filename%.*}.gexf"
-        ./onnx2gexf.py -i "$inputFile" -o "$outputFile"
-    fi
+for f in $(ls $1/*.onnx);
+do
+    onnx2gexf --input $f --mode production --output "$f.gexf"
 done
+
+    # if [ -f "$inputFile" ]; then
+        #get filename w/o extension and save as gexf
+    #     filename=$(basename "$inputFile")
+    #     outputFile="$outputPath/${filename%.*}.gexf"
+    #     ./onnx2gexf.py -i "$inputFile" -o "$outputFile"
+    # fi
