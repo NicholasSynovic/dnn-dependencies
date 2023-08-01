f14902328fc02199b758323df57ac7d666d90310
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-08-01 11:30:05 -0500

    Refactor SQL file

--- dnn_dependencies/schemas/sqlRefactor.py
+++ dnn_dependencies/schemas/sqlRefactor.py
@@ -1,93 +1,92 @@
-from sqlalchemy import (Column, Engine, Float, ForeignKey, Integer, MetaData,
-                        String, Table, create_engine)
+from pathlib import Path
+
+from sqlalchemy import (Column, Connection, Engine, Float, ForeignKey, Integer,
+                        MetaData, String, Table, create_engine)
 
 
-def createEngine(path: str) -> Engine:
-    """
+class SQL:
+    def __init__(self, sqliteDBPath: Path) -> None:
+        sqliteURI: str = f"sqlite:///{sqliteDBPath.absolute.__str__}"
 
+        self.engine: Engine = create_engine(url=sqliteURI)
+        self.conn: Connection = self.engine.connect()
 
-    :param path: str:
+        self.modelPropertiesTableName: str = "Model Properties"
+        self.baseModelsTableName: str = "Base Models"
 
-    """
-    url: str = f"sqlite:///{path}"
-    return create_engine(url=url)
+    def closeConnection(self) -> bool:
+        self.conn.close()
+        return self.conn.closed
 
+    def createSchema_GraphProperties(self) -> MetaData:
+        metadata: MetaData = MetaData()
 
-def schema_ModelStats(metadata: MetaData) -> Table:
-    """
-    NOTE: Changes are made to the metadata object passed into the function
+        Table(
+            self.modelPropertiesTableName,
+            metadata,
+            Column(
+                "ID",
+                Integer,
+                primary_key=True,
+                unique=True,
+                autoincrement=True,
+            ),
+            Column("Model Name", String),
+            Column("Model Filepath", String),
+            Column("Is Semiconnected", String),
+            Column("Is Attracting Component", String),
+            Column("Is Strongly Connected", Integer),
+            Column("Is Weakly Connected", Integer),
+            Column("Is Triad", Integer),
+            Column("Is Regular", Integer),
+            Column("Is Planar", Integer),
+            Column("Is Distance Regular", Integer),
+            Column("Is Strongly Regular", Integer),
+            Column("Is Bipartite", Integer),
+            Column("Is Aperiodic", Integer),
+            Column("Is Directed Acyclic", Integer),
+            Column("Radius", Integer),
+            Column("DAG Longest Path Length", Integer),
+            Column("Number of Isolates", Integer),
+            Column("Robins Alexander Clustering", Float),
+            Column("Transitivity", Float),
+            Column("Number of Nodes", Integer),
+            Column("Density", Float),
+            Column("Number of Edges", Integer),
+            Column("Number of Communities", Integer),
+            Column("Degree Assortivity Coefficient", Float),
+            Column("Attribute Assortivity Coefficient", Float),
+            Column("Number of Weakly Connected Components", Integer),
+            Column("Number of Strongly Connected Components", Integer),
+            Column("Number of Attracting Components", Integer),
+            Column("Barycenter", Integer),
+            Column("Degree Pearson Correlation Coefficient", Float),
+        )
 
+        return metadata
 
-    :param metadata: MetaData:
+    def createSchema_BaseModels(self) -> MetaData:
+        metadata: MetaData = MetaData()
 
-    """
-    table: Table = Table(
-        "ModelStats",
-        metadata,
-        Column(
-            "ID",
-            Integer,
-            primary_key=True,
-            unique=True,
-            autoincrement=True,
-        ),
-        Column("Model Name", String),
-        Column("Model Filepath", String),
-        Column("Is Semiconnected", String),
-        Column("Is Attracting Component", String),
-        Column("Is Strongly Connected", Integer),
-        Column("Is Weakly Connected", Integer),
-        Column("Is Triad", Integer),
-        Column("Is Regular", Integer),
-        Column("Is Planar", Integer),
-        Column("Is Distance Regular", Integer),
-        Column("Is Strongly Regular", Integer),
-        Column("Is Bipartite", Integer),
-        Column("Is Aperiodic", Integer),
-        Column("Is Directed Acyclic", Integer),
-        Column("Radius", Integer),
-        Column("DAG Longest Path Length", Integer),
-        Column("Number of Isolates", Integer),
-        Column("Robins Alexander Clustering", Float),
-        Column("Transitivity", Float),
-        Column("Number of Nodes", Integer),
-        Column("Density", Float),
-        Column("Number of Edges", Integer),
-        Column("Number of Communities", Integer),
-        Column("Degree Assortivity Coefficient", Float),
-        Column("Attribute Assortivity Coefficient", Float),
-        Column("Number of Weakly Connected Components", Integer),
-        Column("Number of Strongly Connected Components", Integer),
-        Column("Number of Attracting Components", Integer),
-        Column("Barycenter", Integer),
-        Column("Degree Pearson Correlation Coefficient", Float),
-    )
-    return table
+        Table(
+            self.baseModelsTableName,
+            metadata,
+            Column(
+                "ID",
+                Integer,
+                primary_key=True,
+                unique=True,
+                autoincrement=True,
+            ),
+            Column(
+                "Model ID",
+                Integer,
+                ForeignKey(f"{self.baseModelsTableName}.ID"),
+                unique=True,
+            ),
+        )
 
+        return metadata
 
-def schema_BaseModels(metadata: MetaData) -> Table:
-    """
-    NOTE: Changes are made to the metadata object passed into the function
-
-
-    :param metadata: MetaData:
-
-    """
-    table: Table = Table(
-        "BaseModels",
-        metadata,
-        Column("ID", Integer, primary_key=True, unique=True, autoincrement=True),
-        Column("Model ID", Integer, ForeignKey("ModelStats.ID"), unique=True),
-    )
-    return table
-
-
-def createTables(metadata: MetaData, engine: Engine) -> None:
-    """
-
-
-    :param metadata: MetaData:
-    :param engine: Engine:
-
-    """
-    metadata.create_all(bind=engine)
+    def createTables(self, metadata: MetaData) -> None:
+        metadata.create_all(bind=self.conn)
