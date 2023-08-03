73e038b08683e7890753c686bc1b0ff4c15951cb
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-08-03 09:23:06 -0500

    Delete old file

--- dnn_dependencies/schemas/sql.py
+++ dnn_dependencies/schemas/sql.py
@@ -1,120 +1,102 @@
-from pathlib import Path
-from typing import Literal
-
-from pandas import DataFrame
-from sqlalchemy import (Column, Connection, Engine, Float, ForeignKey, Integer,
-                        MetaData, String, Table, create_engine)
+from sqlalchemy import (Column, Connection, Float, ForeignKey, Integer,
+                        MetaData, String, Table)
 
 
-class SQL:
-    def __init__(self, sqliteDBPath: Path) -> None:
-        sqliteURI: str = f"sqlite:///{sqliteDBPath.absolute().__str__()}"
+def closeConnection(conn: Connection) -> bool:
+    conn.close()
+    return conn.closed
 
-        self.modelsTableName: str = "Models"
-        self.baseModelsTableName: str = "Base Models"
-        self.modelPropertiesTableName: str = "Model Properties"
 
-        self.metadata: MetaData = MetaData()
+def createSchema_Models(metadata: MetaData, tableName: str = "Models") -> None:
+    Table(
+        tableName,
+        metadata,
+        Column(
+            "ID",
+            Integer,
+            primary_key=True,
+            unique=True,
+            autoincrement=True,
+        ),
+        Column("Model Name", String),
+        Column("Model Filepath", String),
+    )
 
-        self.engine: Engine = create_engine(url=sqliteURI)
-        self.conn: Connection = self.engine.connect()
 
-    def closeConnection(self) -> bool:
-        self.conn.close()
-        return self.conn.closed
+def createSchema_BaseModels(
+    metadata: MetaData,
+    tableName: str = "Base Models",
+    fkTableName: str = "Models",
+) -> None:
+    Table(
+        tableName,
+        metadata,
+        Column(
+            "ID",
+            Integer,
+            primary_key=True,
+            unique=True,
+            autoincrement=True,
+        ),
+        Column(
+            "Model_ID",
+            Integer,
+            ForeignKey(f"{fkTableName}.ID"),
+            unique=True,
+        ),
+    )
 
-    def createSchema_Models(self) -> None:
-        Table(
-            self.modelsTableName,
-            self.metadata,
-            Column(
-                name="ID",
-                type_=Integer,
-                primary_key=True,
-                unique=True,
-                autoincrement=True,
-            ),
-            Column(name="Model Name", type_=String),
-            Column(name="Model Filepath", type_=String),
-            extend_existing=True,
-        )
 
-    def createSchema_BaseModels(self) -> None:
-        Table(
-            self.baseModelsTableName,
-            self.metadata,
-            Column(
-                "ID",
-                Integer,
-                primary_key=True,
-                unique=True,
-                autoincrement=True,
-            ),
-            Column(
-                "Model ID",
-                Integer,
-                ForeignKey(f"{self.modelsTableName}.ID"),
-                unique=True,
-            ),
-        )
-
-    def createSchema_ModelProperties(self) -> None:
-        Table(
-            self.modelPropertiesTableName,
-            self.metadata,
-            Column(
-                "ID",
-                Integer,
-                primary_key=True,
-                unique=True,
-                autoincrement=True,
-            ),
-            Column("Model Name", String),
-            Column("Model Filepath", String),
-            Column("Is Semiconnected", String),
-            Column("Is Attracting Component", String),
-            Column("Is Strongly Connected", Integer),
-            Column("Is Weakly Connected", Integer),
-            Column("Is Triad", Integer),
-            Column("Is Regular", Integer),
-            Column("Is Planar", Integer),
-            Column("Is Distance Regular", Integer),
-            Column("Is Strongly Regular", Integer),
-            Column("Is Bipartite", Integer),
-            Column("Is Aperiodic", Integer),
-            Column("Is Directed Acyclic", Integer),
-            Column("Radius", Integer),
-            Column("DAG Longest Path Length", Integer),
-            Column("Number of Isolates", Integer),
-            Column("Robins Alexander Clustering", Float),
-            Column("Transitivity", Float),
-            Column("Number of Nodes", Integer),
-            Column("Density", Float),
-            Column("Number of Edges", Integer),
-            Column("Number of Communities", Integer),
-            Column("Degree Assortivity Coefficient", Float),
-            Column("Attribute Assortivity Coefficient", Float),
-            Column("Number of Weakly Connected Components", Integer),
-            Column("Number of Strongly Connected Components", Integer),
-            Column("Number of Attracting Components", Integer),
-            Column("Barycenter", Integer),
-            Column("Degree Pearson Correlation Coefficient", Float),
-        )
-
-    def createTables(self) -> None:
-        self.metadata.create_all(bind=self.conn)
-
-    def writeDFToDB(
-        self,
-        df: DataFrame,
-        tableName: Literal["Models", "Base Models", "Model Properties"],
-        keepIndex: bool,
-        indexColumn: str | None = None,
-    ) -> None:
-        df.to_sql(
-            name=tableName,
-            con=self.engine,
-            if_exists="fail",
-            index=keepIndex,
-            index_label=indexColumn,
-        )
+def createSchema_ModelProperties(
+    metadata: MetaData,
+    tableName: str = "Graph Properties",
+    fkTableName: str = "Models",
+) -> None:
+    Table(
+        tableName,
+        metadata,
+        Column(
+            "ID",
+            Integer,
+            primary_key=True,
+            unique=True,
+            autoincrement=True,
+        ),
+        Column(
+            "Model_ID",
+            Integer,
+            ForeignKey(f"{fkTableName}.ID"),
+            unique=True,
+        ),
+        Column("Barycenter", Float),
+        Column("Radius", Float),
+        Column("DAG Longest Path Length", Float),
+        Column("Average Shortest Path Length", Float),
+        Column("Number of Isolates", Float),
+        Column("Is Threshold Graph", Float),
+        Column("Diameter", Float),
+        Column("Attribute Assortativity Coefficient", Float),
+        Column("Degree Pearson Correlation Coefficient", Float),
+        Column("Number of Weakly Connected Components", Float),
+        Column("Number of Strongly Connected Components", Float),
+        Column("Number of Attracting Components", Float),
+        Column("Is Semiconnected", Float),
+        Column("Is Attracting Component", Float),
+        Column("Is Strongly Connected", Float),
+        Column("Is Weakly Connected", Float),
+        Column("Is Triad", Float),
+        Column("Is Regular", Float),
+        Column("Is Planar", Float),
+        Column("Is Distance Regular", Float),
+        Column("Is Strongly Regular", Float),
+        Column("Is Bipartite", Float),
+        Column("Is Aperiodic", Float),
+        Column("Is Directed Acyclic", Float),
+        Column("Robins Alexander Clustering", Float),
+        Column("Transitivity", Float),
+        Column("Number of Nodes", Float),
+        Column("Density", Float),
+        Column("Number of Edges", Float),
+        Column("Number of Communities", Float),
+        Column("Degree Assortivity Coefficient", Float),
+    )
